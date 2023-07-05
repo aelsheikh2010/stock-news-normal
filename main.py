b2a4f5296418a871b880b110ece4f0ba98ed3060
Ahmed Elsheikh <golf17@gmail.com>
2023-07-05 23:25:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,88 @@
+import requests
+from twilio.rest import Client
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+api_stock_key = "P7FEEGDXXECOBSTM"
+NEWS_API_KEY = "1141cd33d2cf46378a5fa38ec79a72d8"
+ACCOUNT_SID = "AC5e38c72883a39c7777692be00c4782bd"
+AUTH_TOKEN = "6553bf7af922f629dd469d8dd4f269b9"
+
+
+
+    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
+# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+
+stock_prams = {
+    "function": "TIME_SERIES_DAILY_ADJUSTED",
+    "symbol": STOCK_NAME,
+    "apikey": api_stock_key
+}
+response = requests.get(STOCK_ENDPOINT, params=stock_prams)
+response.raise_for_status()
+stock_data = response.json()["Time Series (Daily)"]
+daily_data = [value for (key, value) in stock_data.items()]
+yesterday_data = daily_data[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
+
+day_before_yesterday_data = daily_data[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
+#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+#Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+print(diff_percent)
+#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+if abs(diff_percent) >= 1:
+    # print("Get News")
+    #Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+    news_prams = {
+        "q": COMPANY_NAME,
+        "title": COMPANY_NAME,
+        "apiKey": NEWS_API_KEY
+    }
+    response = requests.get(NEWS_ENDPOINT, params=news_prams)
+    response.raise_for_status()
+    articles = response.json()["articles"]
+    #Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    three_articles = articles[:3]
+
+    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
+    #to send a separate message with each article's title and description to your phone number.
+    # Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nheadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    #print(formatted_articles)
+    #Send each article as a separate message via Twilio.
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="+13613106464",
+            to="+201003349095"
+        )
+
+
+
+#Optional TODO: Format the message like this: 
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
